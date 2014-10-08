90cdd391747bc4ccbd62be3b416ed04015664dd4
Takeaki Nagashima <Takeaki Nagashima@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-08 08:56:55 +0000

    ver up
    git-svn-id: https://plugins.svn.wordpress.org/gmo-font-agent/trunk@1003643 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/colorpic.js
@@ -0,0 +1,12 @@
+(function($){
+	$(document).ready(function() {    
+		$('#color_picker__body').farbtastic('#font-color-_body');
+		$('#color_picker_h1').farbtastic('#font-color-h1');
+		$('#color_picker_h2').farbtastic('#font-color-h2');
+		$('#color_picker_h3').farbtastic('#font-color-h3');
+		$('#color_picker_h4').farbtastic('#font-color-h4');
+		$('#color_picker_h5').farbtastic('#font-color-h5');
+		$('#color_picker_h6').farbtastic('#font-color-h6');
+		$('#color_picker__hentry').farbtastic('#font-color-_hentry');
+	});
+})(jQuery);
